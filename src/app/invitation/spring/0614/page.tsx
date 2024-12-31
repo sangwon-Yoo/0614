3ce818886c251e37f33ff5b02f174733984f7249
YooSangwon <ysw1230@gmail.com>
2024-12-31 23:49:29 +0900

    swiper edge

--- src/app/invitation/spring/0614/page.tsx
+++ src/app/invitation/spring/0614/page.tsx
@@ -2,30 +2,57 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import Image from 'next/image';
+import { SwiperModule } from 'swiper/types';
+import { SwiperContainer } from 'swiper/element';
 
 export default function Page0614() {
 
   const [transparentDim, setTransparentDim] = useState(false);
   const [displayDim, setDisplayDim] = useState(true);
+  const [allowSlidePrev, setAllowSlidePrev] = useState(true);
+  const [allowSlideNext, setAllowSlideNext] = useState(true);
 
   useEffect(() => {
     setTransparentDim(true);
     setTimeout(() => setDisplayDim(false), 2000);
   }, []);
 
+  const blockSwipeOnEdge = useCallback((swiper: SwiperContainer['swiper']) => {
+    if(swiper.activeIndex == 0) {
+      setAllowSlidePrev(false);
+    } else if(swiper.activeIndex == swiper.slides.length - 1) {
+      setAllowSlideNext(false);
+    } else {
+      setAllowSlidePrev(true);
+      setAllowSlideNext(true);
+    }
+  }, []);
+
   return (
     <>
       <div className={`${displayDim ? 'block' : 'hidden'} fixed z-50 inset-0 transition-opacity ease-linear delay-1000 duration-1000 bg-white ${transparentDim ? 'opacity-0' : ''}`} />
       <Swiper
+        allowSlidePrev={allowSlidePrev}
+        allowSlideNext={allowSlideNext}
         direction={'vertical'}
+        resistance={false}
         slidesPerView={1}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={(swiper) => blockSwipeOnEdge(swiper)}
+        onSwiper={(swiper) => blockSwipeOnEdge(swiper)}
         className={'h-full'}
       >
         <SwiperSlide>
-          <div  className={''}>11111</div>
+          <div className={'relative h-full'}>
+            <Image
+              fill={true}
+              src="/gallery/main.jpg"
+              sizes="(max-width: 640px) 100vw"
+              alt={'메인 사진'}
+              className={'object-cover'}
+            />
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={''}>2222</div>
