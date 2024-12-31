24a0ab78ee942bbea5c88ec0a7643ddefb11c321
YooSangwon <ysw1230@gmail.com>
2024-12-31 22:47:52 +0900

    ++layout

--- src/app/invitation/spring/0614/page.tsx
+++ src/app/invitation/spring/0614/page.tsx
@@ -2,28 +2,41 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { useEffect, useState } from 'react';
 
 export default function Page0614() {
+
+  const [transparentDim, setTransparentDim] = useState(false);
+  const [displayDim, setDisplayDim] = useState(true);
+
+  useEffect(() => {
+    setTransparentDim(true);
+    setTimeout(() => setDisplayDim(false), 2000);
+  }, []);
+
   return (
-    <Swiper
-      direction={'vertical'}
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      className={'h-screen'}
-    >
-      <SwiperSlide>
-        <div  className={'h-full bg-amber-100'}>11111</div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className={'h-full bg-amber-200'}>2222</div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className={'h-full bg-amber-100'}>3333</div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className={'h-full bg-amber-100'}>4444</div>
-      </SwiperSlide>
-    </Swiper>
+    <>
+      <div className={`${displayDim ? 'block' : 'hidden'} fixed z-50 inset-0 transition-opacity ease-linear delay-1000 duration-1000 bg-white ${transparentDim ? 'opacity-0' : ''}`} />
+      <Swiper
+        direction={'vertical'}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        className={'h-full'}
+      >
+        <SwiperSlide>
+          <div  className={''}>11111</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={''}>2222</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={''}>3333</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={''}>4444</div>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
--- src/app/invitation/spring/template.tsx
+++ src/app/invitation/spring/template.tsx
@@ -1,5 +1,22 @@
 import { ReactNode } from 'react'
+import { Geist, Geist_Mono } from 'next/font/google';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
 export default function TemplateSpring({ children }: { children: ReactNode }) {
-  return children;
+  return (
+    <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-row justify-center bg-rose-50`}>
+      <div className={'h-screen w-screen max-w-screen-sm'}>
+        {children}
+      </div>
+    </body>
+  );
 }
