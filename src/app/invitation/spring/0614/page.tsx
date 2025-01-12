6571d2f6fb611c4a35ee0ce5c088e8058a25cc1f
YooSangwon <ysw1230@gmail.com>
2025-01-12 17:21:38 +0900

    add animation

--- src/app/invitation/spring/0614/page.tsx
+++ src/app/invitation/spring/0614/page.tsx
@@ -45,7 +45,7 @@ export default function Page0614() {
         slidesPerView={1}
         onSlideChange={(swiper) => blockSwipeOnEdge(swiper)}
         onSwiper={(swiper) => blockSwipeOnEdge(swiper)}
-        className={'h-full'}
+        className={'h-svh'}
       >
         <SwiperSlide>
           <div className={'relative h-full'}>
@@ -74,10 +74,10 @@ export default function Page0614() {
               />
             </div>
 
-            <div className={'absolute text-lg font-medium w-full flex flex-col items-center bottom-20 text-white'}>
+            <div className={'absolute text-lg font-medium w-full flex flex-col items-center bottom-10 text-white'}>
               <div>허현정 &#x2665; 유상원</div>
-              <div>2025년 6월 14일</div>
-              <div>엘리에나 호텔, 3F, 임페리얼 홀</div>
+              <div>2025년 6월 14일 오후 2시 30분</div>
+              <div>엘리에나 호텔, 3F 임페리얼 홀</div>
             </div>
           </div>
         </SwiperSlide>
@@ -86,8 +86,14 @@ export default function Page0614() {
             <div className={'flex flex-col justify-center items-center flex-none h-24'}>
               <span className={'font-greatVibes text-lg'}>Welcome</span>
             </div>
-            <div className={'flex flex-col flex-1'}>
-              <div className={'flex-auto'}>
+            <div className={'flex flex-col justify-between flex-1'}>
+              <motion.div
+                className={'flex-none'}
+                initial={{ y: 25, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, ease: 'easeOut' }}
+              >
                 <p className={'whitespace-nowrap text-center leading-8 mb-6'}>
                   {'저희 두 사람이'}<br />
                   {'평생을 함께하기 위해'}<br />
@@ -97,31 +103,65 @@ export default function Page0614() {
                   {'그 진실 서약을 하는 자리에'}<br />
                   {'소중한 분들을 모십니다.'}<br />
                 </p>
-                <div className={'flex flex-row justify-center'}>
-                  <p className={'whitespace-nowrap mb-6 flex-none w-48 leading-7'}>
-                    <span className={'font-bold'}>{' 유 진 재 '}</span>
-                    &#183;
-                    <span className={'font-bold'}>{' 윤 혜 정 '}</span>
-                    {'의 아들'}
-                    <br />
-                    <span className={'font-bold'}>{' 허 만 인 '}</span>
-                    &#183;
-                    <span className={'font-bold'}>{' 이 정 자 '}</span>
-                    {'의 딸'}
-                  </p>
-                  <p className={'whitespace-nowrap mb-6 flex-none w-14 leading-7'}>
-                    <span className={'font-bold'}>{' 유 상 원 '}</span>
-                    <br />
-                    <span className={'font-bold'}>{' 허 현 정 '}</span>
-                  </p>
-                </div>
+              </motion.div>
+              <div className={'flex-none flex justify-center'}>
+                <Image
+                  src="/image/bouquet/bouquet-1.png"
+                  alt={'bouquet1'}
+                  sizes="(max-width: 640px) 124px"
+                  width={1}
+                  height={1}
+                  style={{
+                    width: '124px',
+                    height: 'auto',
+                  }}
+                />
               </div>
-              <div className={'flex-auto'}></div>
+              <motion.div
+                className={'flex-none flex flex-row justify-center'}
+                initial={{ y: 25, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, ease: 'easeOut' }}
+              >
+                <p className={'whitespace-nowrap mb-6 flex-none w-48 leading-7'}>
+                  <span className={'font-bold'}>{' 유 진 재 '}</span>
+                  &#183;
+                  <span className={'font-bold'}>{' 윤 혜 정 '}</span>
+                  {'의 아들'}
+                  <br />
+                  <span className={'font-bold'}>{' 허 만 인 '}</span>
+                  &#183;
+                  <span className={'font-bold'}>{' 이 정 자 '}</span>
+                  {'의 딸'}
+                </p>
+                <p className={'whitespace-nowrap mb-6 flex-none w-14 leading-7'}>
+                  <span className={'font-bold'}>{' 유 상 원 '}</span>
+                  <br />
+                  <span className={'font-bold'}>{' 허 현 정 '}</span>
+                </p>
+              </motion.div>
+              <motion.div
+                className={'flex-none'}
+                initial={{ y: 25, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, ease: 'easeOut' }}
+              >
+                <p className={'whitespace-nowrap text-center leading-8 mb-6'}>
+                  {'2025년 6월 14일 오후 2시 30분'}<br />
+                  {'엘리에나 호텔, 3F 임페리얼 홀'}<br />
+                </p>
+              </motion.div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className={''}>3333</div>
+          <div className={'relative h-full flex flex-col'}>
+            <div className={'flex flex-col justify-center items-center flex-none h-24'}>
+              <span className={'font-greatVibes text-lg'}>Date</span>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={''}>4444</div>
