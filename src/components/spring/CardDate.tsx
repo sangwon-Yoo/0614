8549c6b220b20bc8481c7613ce67b23a653923f1
YooSangwon <ysw1230@gmail.com>
2025-01-13 17:01:27 +0900

    ++date card

--- src/components/spring/CardDate.tsx
+++ src/components/spring/CardDate.tsx
@@ -1,10 +1,30 @@
 export default function CardDate() {
+
+  const monthDays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+
   return (
     <div className={'relative h-full flex flex-col'}>
       <div
         className={'flex flex-col justify-center items-center flex-none h-24'}
       >
         <span className={'font-greatVibes text-lg'}>Date</span>
+      </div>
+      <div className={'flex flex-col flex-1'}>
+
+        <div className={'flex justify-center items-center flex-initial h-1/6 bg-blue-300'}>
+          <span>2025년 06월</span>
+        </div>
+
+        <div className={'flex-initial h-3/6 bg-blue-700'}>
+          <div className={'grid grid-cols-7'}>
+            {monthDays.map((day, index) => <div key={index}>{day}</div>)}
+          </div>
+        </div>
+
+        <div className={'flex-initial h-2/6 bg-blue-100'}>
+          D-78
+        </div>
+
       </div>
     </div>
   );
