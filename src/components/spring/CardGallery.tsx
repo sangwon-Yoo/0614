3f0f9768261c978289a5e9e32ef05e8c72b7447b
YooSangwon <ysw1230@gmail.com>
2025-01-25 18:13:16 +0900

    modal

--- src/components/spring/CardGallery.tsx
+++ src/components/spring/CardGallery.tsx
@@ -1,6 +1,25 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useSwiper } from 'swiper/react';
 
 export default function CardGallery() {
+
+  const [isPopGallery, setIsPopGallery] = useState<boolean>(false);
+  const [tranparentModal, setTranparentModal] = useState(true);
+
+  const swiper = useSwiper();
+
+  useEffect(() => {
+    if(isPopGallery) {
+      setTranparentModal(false);
+      swiper.disable();
+    }
+    else {
+      setTranparentModal(true);
+      swiper.enable();
+    }
+  }, [isPopGallery]);
+
 
   return (
     <div className={'relative h-full flex flex-col'}>
@@ -14,7 +33,7 @@ export default function CardGallery() {
           <p className={'text-center'}>사진을 눌러 보세요.</p>
         </div>
         <div className={'grid grid-rows-4 grid-cols-2 grid-flow-col gap-3 flex-1 mx-3 my-8'}>
-          <div className={'relative bg-blue-200'}>
+          <button onClick={() => setIsPopGallery(true)} className={'relative bg-blue-200'}>
             <Image
               fill={true}
               src="/gallery/cards/safari01.jpg"
@@ -22,8 +41,8 @@ export default function CardGallery() {
               alt={'메인 사진'}
               className={'object-cover rounded-md'}
             />
-          </div>
-          <div className={'relative row-span-2 bg-blue-200'}>
+          </button>
+          <button onClick={() => setIsPopGallery(true)} className={'relative row-span-2 bg-blue-200'}>
             <Image
               fill={true}
               src="/gallery/cards/safari02.jpeg"
@@ -31,8 +50,8 @@ export default function CardGallery() {
               alt={'메인 사진'}
               className={'object-cover rounded-md'}
             />
-          </div>
-          <div className={'relative bg-blue-200'}>
+          </button>
+          <button onClick={() => setIsPopGallery(true)} className={'relative bg-blue-200'}>
             <Image
               fill={true}
               src="/gallery/cards/safari03.jpg"
@@ -40,8 +59,8 @@ export default function CardGallery() {
               alt={'메인 사진'}
               className={'object-cover rounded-md'}
             />
-          </div>
-          <div className={'relative row-span-2 bg-blue-200'}>
+          </button>
+          <button onClick={() => setIsPopGallery(true)} className={'relative row-span-2 bg-blue-200'}>
             <Image
               fill={true}
               src="/gallery/cards/safari04.jpeg"
@@ -49,8 +68,8 @@ export default function CardGallery() {
               alt={'메인 사진'}
               className={'object-cover rounded-md'}
             />
-          </div>
-          <div className={'relative row-span-2 bg-blue-200'}>
+          </button>
+          <button onClick={() => setIsPopGallery(true)} className={'relative row-span-2 bg-blue-200'}>
             <Image
               fill={true}
               src="/gallery/cards/safari05.jpeg"
@@ -58,9 +77,16 @@ export default function CardGallery() {
               alt={'메인 사진'}
               className={'object-cover rounded-md'}
             />
-          </div>
+          </button>
         </div>
       </div>
+
+      {isPopGallery && (
+        <div
+          className={`fixed z-50 inset-0 transition-opacity ease-linear duration-100 bg-black ${tranparentModal ? 'opacity-0' : 'opacity-100'}`}
+          onClick={() => setIsPopGallery(false)}
+        />
+      )}
     </div>
   );
 }
