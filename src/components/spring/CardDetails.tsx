cce79cd04ea1b99853a34405996b254b8731611d
YooSangwon <ysw1230@gmail.com>
2025-03-04 09:53:49 +0900

    --불필요 import

--- src/components/spring/CardDetails.tsx
+++ src/components/spring/CardDetails.tsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-import Image from 'next/image';
 
 export default function CardDetails({currentSlideIndex}: {currentSlideIndex: number}) {
 
