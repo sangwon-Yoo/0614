4afbfd564e05952e70f0abf59040354530638c2d
YooSangwon <ysw1230@gmail.com>
2025-04-07 17:25:32 +0900

    오픈

--- src/app/invitation/spring/0614/page.tsx
+++ src/app/invitation/spring/0614/page.tsx
@@ -1,7 +1,9 @@
+import InitServerData from '@/components/spring/server/initServerData';
+
 export default function Page0614() {
 
-  /*return <InitServerData />;*/
-  return (
+  return <InitServerData />;
+  /*return (
     <div className={'h-full flex flex-col justify-center items-center font-greatVibes text-lg'}>Coming Soon ❤️</div>
-  );
+  );*/
 }
