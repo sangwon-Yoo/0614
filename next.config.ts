35ad593897450456655efc8d0e8fd9bcacf4a392
YooSangwon <ysw1230@gmail.com>
2025-04-14 17:24:39 +0900

    리다이렉트 로직 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/invitation/spring/0614',
+        permanent: false, // 307 (임시 이동), true로 하면 308
+      },
+    ];
+  },
 };
 
 export default nextConfig;
